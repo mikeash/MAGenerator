b47d2f02d51321a0b49c7954f76f61848c8fcc0d
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2009-10-23 21:46:37 +0000

    MAGenerator: added HTTP parser example
    git-svn-id: http://mikeash.com/svn/MAGenerator@80 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- GeneratorTests.m
+++ GeneratorTests.m
@@ -85,6 +85,134 @@ GENERATOR(int, Counter(int start, int end), (void))
     GENERATOR_END
 }
 
+void AppendByte(NSMutableData *data, char byte)
+{
+    [data appendBytes: &byte length: 1];
+}
+
+NSString *SafeUTF8String(NSData *data)
+{
+    NSString *str = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    if(!str)
+        str = [[NSString alloc] initWithData: data encoding: NSISOLatin1StringEncoding];
+    if(!str)
+        str = [[NSString alloc] initWithData: data encoding: NSMacOSRomanStringEncoding];
+    return str;
+}
+
+GENERATOR(int, HTTPParser(void (^responseCallback)(NSString *), void (^headerCallback)(NSDictionary *), void (^bodyCallback)(NSData *), void (^errorCallback)(NSString *)), (int byte))
+{
+    NSMutableData *responseData = [NSMutableData data];
+    
+    NSMutableDictionary *headers = [NSMutableDictionary dictionary];
+    __block NSMutableData *currentHeaderData = nil;
+    
+    NSMutableData *bodyData = [NSMutableData data];
+    
+    GENERATOR_BEGIN(char byte)
+    {
+        while(byte != '\r')
+        {
+            AppendByte(responseData, byte);
+            GENERATOR_YIELD(0);
+        }
+        responseCallback(SafeUTF8String(responseData));
+        GENERATOR_YIELD(0); // eat the \r
+        if(byte != '\n')
+            errorCallback(@"bad CRLF after response line");
+        GENERATOR_YIELD(0); // eat the \n
+        
+        while(1)
+        {
+            currentHeaderData = [[NSMutableData alloc] init];
+            while(byte != '\r')
+            {
+                AppendByte(currentHeaderData, byte);
+                GENERATOR_YIELD(0);
+            }
+            GENERATOR_YIELD(0);
+            if(byte != '\n')
+                errorCallback(@"bad CRLF after header line");
+            GENERATOR_YIELD(0); // eat the \n
+            
+            NSString *headerString = SafeUTF8String(currentHeaderData);
+            if([headerString length])
+            {
+                NSUInteger colonLoc = [headerString rangeOfString: @": "].location;
+                if(colonLoc == NSNotFound)
+                    errorCallback(@"No colon found in header line");
+                [headers setObject: [headerString substringFromIndex: colonLoc + 2] forKey: [headerString substringToIndex: colonLoc]];
+            }
+            else
+                break;
+        }
+        headerCallback(headers);
+        
+        while(byte != -1)
+        {
+            AppendByte(bodyData, byte);
+            GENERATOR_YIELD(0);
+        }
+        bodyCallback(bodyData);
+    }
+    GENERATOR_CLEANUP
+    {
+        [currentHeaderData release];
+    }
+    GENERATOR_END
+}
+
+void TestHTTP(void)
+{
+    NSInputStream *is;
+    NSOutputStream *os;
+    [NSStream getStreamsToHost: [NSHost hostWithName: @"www.google.com"] port: 80 inputStream: &is outputStream: &os];
+    
+    [is open];
+    [os open];
+    
+    char *writeBytes = "GET / HTTP/1.0\r\n\r\n";
+    NSInteger toWrite = strlen(writeBytes);
+    while(toWrite)
+    {
+        NSInteger written = [os write: (uint8_t *)writeBytes maxLength: toWrite];
+        if(written < 0)
+        {
+            perror("write");
+            exit(1);
+        }
+        toWrite -= written;
+        writeBytes += written;
+    }
+    
+    int (^parser)(int) = HTTPParser(
+                                     ^(NSString *response) {
+                                         NSLog(@"Got response: %@", response);
+                                     },
+                                     ^(NSDictionary *headers) {
+                                         NSLog(@"Got headers: %@", headers);
+                                     },
+                                     ^(NSData *body) {
+                                         NSLog(@"Got %ld bytes of body", (long)[body length]);
+                                     },
+                                     ^(NSString *error) {
+                                         NSLog(@"Got error %@", error);
+                                     });
+    
+    uint8_t byte;
+    NSInteger amt;
+    while((amt = [is read: &byte maxLength: 1]))
+    {
+        if(amt < 0)
+        {
+            perror("read");
+            exit(1);
+        }
+        parser(byte);
+    }
+    parser(-1);
+}
+
 int main(int argc, char **argv)
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
